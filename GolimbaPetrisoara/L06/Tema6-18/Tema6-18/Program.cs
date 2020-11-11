8ac381e563eec3217ded5f8a362bf6ff4f84dcb2
pgolimba <petrisaora.golimba@student.upt.ro>
2020-11-11 22:37:18 +0200

    tema6

---
+++ GolimbaPetrisoara/L06/Tema6-18/Tema6-18/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tema6_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var wf = from createReplyResult in BoundedContextDSL.ValidateReply(100, 1, "mesaj de test")
+                let validReply = (CreateReplyResult.ReplyValid)createReplyResult
+                from checkLanguageResult in BoundedContextDSL.CheckLanguage(validReply.Reply.Answer)
+                from ownerAck in BoundedContextDSL.SendAckToOwner(checkLanguageResult)
+                from authorAck in BoundedContextDSL.SendAckToAuthor(checkLanguageResult)
+                select (validReply, checkLanguageResult, ownerAck, authorAck);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
